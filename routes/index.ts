4feb3a45a98ca4fef59cf9a92dc517cda7e5232f
zirina <JayChouZirina@gmail.com>
2023-03-13 23:39:33 +0800

    feat: 新增 register 路由

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,13 @@
-import express from "express";
-const router = express.Router();
+import bodyParser from 'body-parser'
+import express from 'express'
+import { createUser } from '../controller/user'
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('!!!');
-});
+router.get('/', function (req, res, next) {
+  res.send('!!!')
+})
+
+router.post('/register', bodyParser.json(), createUser)
 
 export default router
