6d258bf7ff34a97ef2d9c5c01f2e2770bd02ab32
zirina <JayChouZirina@gmail.com>
2023-03-13 23:38:59 +0800

    feat: 更新 users 接口

--- controller/user.ts
+++ controller/user.ts
@@ -1,62 +1,46 @@
-import UserModel, { IUserCreationAttributes } from '../models/user'
-import { NextFunction, Request, Response } from 'express'
+import UserModel, { type IUserCreationAttributes } from '../models/user'
+import type { NextFunction, Request, Response } from 'express'
 import bcrypt from 'bcrypt'
+import type { ExpressMiddlewareWithRequest, ExpressMiddleware } from '../type/express'
+import { checkMissingParams } from '../utils'
 
 const salt = bcrypt.genSaltSync(10)
 
-type ExpressMiddleware<T> =  (
-  req: Request & T, 
-  res: Response, 
-  next: NextFunction
-) => void
-
 interface IUser {
-  userName?: string,
-  password?: string,
+  userName?: string
+  password?: string
   email?: string
 }
 
-function checkMissingParams(...params: any[]) {
-  const res = params.reduce((sum, param) => {
-    console.log(param, !!param)
-    return sum + !!param
-  }, 0)
-  return res !== params.length
-}
-
-export const createUser: ExpressMiddleware<IUser> = async (req, res, next) => {
-  const { userName, password, email } = req?.body
+export const createUser: ExpressMiddlewareWithRequest<IUser> = async (req, res) => {
+  const { userName, password, email } = req.body ?? {}
   const missingParams = checkMissingParams(userName, password, email)
 
-  if(missingParams) {
+  if (missingParams) {
     res.send({
       code: 422,
       msg: '参数错误'
     })
     return
-  } 
+  }
 
-  const newUser = {
+  const newUser: IUserCreationAttributes = {
     userName,
     password: bcrypt.hashSync(password, salt),
-    email,
-  } as IUserCreationAttributes
-  
+    email
+  }
+
   try {
     const result = await UserModel.create(newUser)
     const { userId } = result
-    if(result) {
-      res.send({ 
-        code: 200, 
-        data: {
-          userId,
-        },
-        msg: '创建成功' 
-      })
-    } else {
-      throw new Error('Unknown Error')
-    }
-  } catch(err) {
+    res.send({
+      code: 200,
+      data: {
+        userId
+      },
+      msg: '创建成功'
+    })
+  } catch (err) {
     console.log(err)
     res.send({
       code: 500,
@@ -65,13 +49,21 @@ export const createUser: ExpressMiddleware<IUser> = async (req, res, next) => {
   }
 }
 
-export const getUserList = async (req: Request, res: Response, next: NextFunction) => {
-  const data = await UserModel.findAll()
-
-      
+export const getUserList: ExpressMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const data = await UserModel.findAll({
+    where: {
+      'isDelete': false
+    },
+    attributes: ['userId', 'userName', 'email']
+  })
+  res.send({
+    code: 200,
+    msg: 'success',
+    data
+  })
 }
 
 export default {
   create: createUser,
-  list: getUserList,
+  list: getUserList
 }
--- routes/users.ts
+++ routes/users.ts
@@ -1,14 +1,9 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-import UserController, { createUser } from '../controller/user'
-var router = express.Router();
+import { getUserList } from '../controller/user'
+const router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.post('/register', bodyParser.json(), createUser)
+router.get('/', getUserList)
 
 export default router
 
