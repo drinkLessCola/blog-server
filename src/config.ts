7e6d0effb5d1aa45cea92dc54a5a9394ef30dd65
zirina <JayChouZirina@gmail.com>
2023-03-23 11:04:16 +0800

    build: 增加环境变量设置

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const NODE_ENV = process.env.NODE_ENV ?? 'development'
+export const HOST = process.env.HOST ?? 'localhost'
+export const PORT = process.env.PORT ?? 3000
+
+export const DATABASE_NAME = process.env.DATABASE_NAME ?? 'blog'
+export const DATABASE_HOST = process.env.DATABASE_HOST ?? 'localhost'
+export const DATABASE_PORT = Number(process.env.DATABASE_PORT) ?? 3306
+export const DATABASE_USERNAME = process.env.DATABASE_USERNAME ?? 'root'
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
